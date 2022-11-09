20fa8c588adaad044070c15cf6eaf7483c62797f
203 <203@academicos.cesde.edu.co>
2022-11-09 08:43:58 -0500

    PMM15-se comienza a trabajar en el tercer momento de moviles

---
+++ componentes/Navigation/BottonTabNavigator.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons"
+import Home from '../Screen/Home';
+import Vendedor from '../Screen/Vendedor';
+import Ventas from '../Screen/Ventas'
+
---
+++ componentes/Screen/Home.js
@@ -0,0 +1,8 @@
+import { View, Text, Image, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { useState } from 'react'
+import { useForm, Controller } from 'react-hook-form'
+
+export default function Home({navigation, route}) {
+    
+}
